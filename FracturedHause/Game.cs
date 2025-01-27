2819be3967e7acf113dddd863555003dfd6c8622
baran ipek <baranipk2014@gmail.com>
2025-01-27 16:22:44 +0300

    ver0.4

--- FracturedHause/Game.cs
+++ FracturedHause/Game.cs
@@ -15,7 +15,8 @@ namespace FracturedHause
         #region Items
         Item note0 = new Item(1, "Note - UNKOWN", "GET OUT OF THIS HOUSE\r\nTHIS HOUSE IS CURSED\r\nGET OUT OF THIS HOUSE\r\nTHIS HOUSE IS CURSED\r\nGET OUT OF THIS HOUSE\r\nTHIS HOUSE IS CURSED\r\nGET OUT OF THIS HOUSE\r\nTHIS HOUSE IS CURSED\r\nTHIS HOUSE IS A CURSE\r\nCURSE\r\nTHIS HOUSE\r\nHOUSE\r\nDEATH.");
         Item note1 = new Item(2, "Note - For BAPHOMET", "3 deer hearts\r\n4 human scalps\r\n2 rabbit feet\r\nBoil slowly.\r\n4 liters of cow's blood\r\n3 human molars\r\n2 Datura stramonium\r\n1 oak tree root\r\n3 liters of water\r\nCow intestines\r\nRabbit liver\r\nRabbit ear");
-        
+        Item note2 = new Item(3, "SATANİST'S BLESSİNG", "OH GREAT SATAN\r\nOH GREAT SATAN\r\nBLESSED BE US\r\nBLESSED BE US\r\nBLESS US WITH PAIN\r\nBLESS US WITH SORROW\r\nBLESS US WITH DEATH\r\nOH GREAT SATAN\r\nOH GREAT SATAN\r\nI DEDICATE MY FLESH TO YOU\r\nI DEDICATE MY HEART TO YOU\r\nI DEDICATE MY BLOOD TO YOU\r\nI DEDICATE MY LIVER TO YOU\r\nI DEDICATE MY BRAIN TO YOU\r\nI DEDICATE MYSELF TO YOU\r\nMAKE ME ONE OF YOUR DEMONS");
+        Item key1 = new Item(4, "The KEY", "The KEY : The key is made of a material similar to stainless steel and shows no signs of corrosion. When you hold it, it feels 2-3 times heavier than it should be. The circle at the end appears to be crafted with extremely fine workmanship.");
         #endregion
 
         public void StartGame()
@@ -83,7 +84,7 @@ namespace FracturedHause
                 }
                 else if (input.ToLower().Trim() == "3")
                 {
-                    Console.WriteLine();
+                    Bathroom();
                 }
                 else if (input.ToLower().Trim() == "4")
                 {
@@ -123,9 +124,9 @@ namespace FracturedHause
             while (true)
             {
                 Console.WriteLine("Seçenekler");
-                Console.WriteLine("1 Not");
-                Console.WriteLine("2 KAZAN");
-                Console.WriteLine("3 KÖMÜRLÜK");
+                Console.WriteLine("1 Note");
+                Console.WriteLine("2 The pot");
+                Console.WriteLine("3 Coal shed");
                 Console.WriteLine("4 Inventory");
                 Console.WriteLine("5 höle Geri Dön");
                 Console.WriteLine(hypen);
@@ -168,14 +169,14 @@ namespace FracturedHause
         {
             Console.WriteLine(" yatak odası Grildi");
             Console.WriteLine(hypen);
-            Console.WriteLine("Bedrumyazısı");
+            Console.WriteLine("When you enter the bedroom, you are faced with a large bed surrounded by a drawn circle and a star. There are bloodstains on the bed, candles, a large vase, a key, and a painting on the mirror. In the painting, there are four young girls, one man, and a demon behind the man. The demon’s horns are long and twisted, its legs resemble those of a satyr, with a goat-like appearance. Its upper body is bear-like, covered in thick fur, and its large hands are claw-shaped, but its face resembles a human’s. The bed is completely empty, with the springs exposed, and it reeks of urine. As you look around, you find a key. On the key, it says \"Basement,\" and the end of the key is shaped like a satanic circle. You don’t find anything else in the room.");
             Console.WriteLine(hypen);
             while (true)
             {
                 Console.WriteLine("Seçenekler");
-                Console.WriteLine("1 item");
-                Console.WriteLine("2 eşya1");
-                Console.WriteLine("3 eşya2");
+                Console.WriteLine("1 Key");
+                Console.WriteLine("2 Vase");
+                Console.WriteLine("3 Painting");
                 Console.WriteLine("4 Inventory");
                 Console.WriteLine("5 Back");
                 Console.WriteLine(hypen);
@@ -184,17 +185,18 @@ namespace FracturedHause
                 {
                     if (choice == 1)
                     {
-                        inventory.AddItem(null);
+                        inventory.AddItem(key1);
+                        isBasmentUnlocked = true;
                         Console.WriteLine(hypen);
                     }
                     else if (choice == 2)
                     {
-                        Console.WriteLine("");
+                        Console.WriteLine("Vase: The surface of the vase is filled with tiles and carvings, creating gaps. Inside, there is a yellow liquid resembling urine. This must be the source of the smell.");
                         Console.WriteLine(hypen);
                     }
                     else if (choice == 3)
                     {
-                        Console.WriteLine("");
+                        Console.WriteLine("Painting: The painting is approximately 150 cm long and 110 cm wide. It contains four girls, one man, and a demon. The girls appear mostly happy, while the man looks very tense, and the demon is grinning.");
                         Console.WriteLine(hypen);
                     }
                     else if (choice == 4)
@@ -216,7 +218,52 @@ namespace FracturedHause
 
         private void Bathroom()
         {
-
+            Console.WriteLine(" Banyoya Girildi");
+            Console.WriteLine(hypen);
+            Console.WriteLine("You proceed to the bathroom, a broken mirror, a toilet and a bathtub meet you in the bathroom, where there should be a washbasin, there is a gap and a trace left by the old washbasin, you look around, a number of tools catch your eye on the floor, a scalpel, bandages and bone fragments from the bathtub. There is a very intense odour, when you move to the bathtub, you see a body part lying in it, a human body with no body parts above the waist welcomes you, the internal organs are completely removed, only one undercarriage, you want to leave the room in panic, but you look around with a curiosity you cannot understand, you find another note.");
+            Console.WriteLine(hypen);
+            while (true)
+            {
+                Console.WriteLine("Seçenekler");
+                Console.WriteLine("1 Note");
+                Console.WriteLine("2 Items on the floor");
+                Console.WriteLine("3 Bathtub");
+                Console.WriteLine("4 Inventory");
+                Console.WriteLine("5 Back");
+                Console.WriteLine(hypen);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int choice))
+                {
+                    if (choice == 1)
+                    {
+                        inventory.AddItem(note2);
+                        Console.WriteLine(hypen);
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.WriteLine("Items on the floor: The medical supplies on the floor seem to have been used for cutting and binding something.");
+                        Console.WriteLine(hypen);
+                    }
+                    else if (choice == 3)
+                    {
+                        Console.WriteLine("Bathtub: There is no blood in the bathtub, but there are body parts, and it smells disgusting.");
+                        Console.WriteLine(hypen);
+                    }
+                    else if (choice == 4)
+                    {
+                        inventory.OpenInventory();
+                    }
+                    else if (choice == 5)
+                    {
+                        Console.WriteLine("Hole Geri Dönülüyor...");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen bir sayı girin!");
+                }
+            }
         }
 
         private void Basement()
